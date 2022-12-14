b7e4f0d43995f51f374082321656dacb93e31276
Lloyd Chapman <lacchapman@gmail.com>
2022-12-14 23:10:05 +0000

    Set burn-in to remove and calculate parameter posterior quantiles

--- R/fit_FP.R
+++ R/fit_FP.R
@@ -11,6 +11,7 @@ library(readxl)
 library(lubridate)
 library(ISOweek)
 library(ggplot2)
+library(GGally)
 library(MASS)
 library(abind)
 
@@ -22,6 +23,7 @@ source("R/simulate.R")
 source("R/fit_covid_multi_strain.R")
 source("R/pmcmc.R")
 source("R/plot_fit.R")
+source("R/process_fit.R")
 
 # Process FP data
 source("R/process_FP_data.R")
@@ -36,11 +38,21 @@ u <- c(1:5,7:9,12:13) # beta parameters, seed date, strain seed date, IHR scalin
 # u <- c(1:10,12:14,17) # beta parameters, seed date, strain seed date, IHR scaling, 2nd strain seed date, observation parameters for case data
 # u <- c(1:7,9:11,14) # beta parameters, seed date, strain seed date, IHR scaling, 2nd strain seed date, observation parameters for case data
 n_iters <- 5e4 #1e3 #2e4 #1e4 #
-run <- 71
+run <- 76
 deterministic <- T # flag for whether to use "deterministic particle filter" or not
 Rt <- T #F # flag for whether to return variables needed for calculating Rt in "state"
 thinning <- 10
 fit_covid_multi_strain(u,n_iters,run,deterministic,Rt,thinning)
 
+# Set burn-in
+burnin <- 1500
+
+# Set whether to plot moving average of data
+moving_avg <- F
+
 # Plot fit
-plot_fit(paste0("output/MCMCoutput",run,".RData"),run)
+plot_fit(paste0("output/MCMCoutput",run,".RData"),run,burnin,moving_avg)
+
+# Process fit
+pars_qntls <- calculate_parameter_quantiles(paste0("output/MCMCoutput",run,".RData"),burnin = burnin)
+write.csv(pars_qntls,paste0("output/parameter_quantiles",run,".csv"), row.names = F)
