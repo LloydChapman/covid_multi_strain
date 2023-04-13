8d345120293b76ca48d6803abf2f710536501b7f
Lloyd Chapman <lacchapman@gmail.com>
2023-04-13 18:13:08 +0100

    Tidy up fit_FP

--- R/fit_FP.R
+++ R/fit_FP.R
@@ -28,34 +28,10 @@ source("R/pmcmc.R")
 source("R/plot_fit.R")
 source("R/process_fit.R")
 
-# # Process FP data
-# source("R/process_FP_data.R")
-
 # Load data
 data_raw <- fread("data/data_cases_hosps_deaths_serology.csv")
 vax <- fread("data/data_vaccination.csv", colClasses = c(number = "numeric"))
 pop <- fread("data/population.csv")
-
-# # Set end date for data
-# end_date <- as.Date("2022-05-06") # last death date in data files
-# 
-# # Set age groups
-# age_groups <- c("0-9","10-19","20-29","30-39","40-49","50-59","60-69","70+")
-# 
-# # Create vaccination schedule
-# # Set delays for immune response to different vaccine doses
-# delay_dose1 <- 28
-# delay_dose2 <- 14
-# 
-# # Get vaccination age groups
-# age_groups_vax <- vax[,unique(age_group)]
-# 
-# # Matrix of uptake rates (age group x dose)
-# uptake <- matrix(1,nrow = length(age_groups),ncol = vax[,length(unique(dose))])
-# 
-# # Make vaccine schedule
-# schedule <- vaccination_data(vax,delay_dose1,delay_dose2,pop,age_groups_vax,
-#                              age_groups,end_date,uptake)
 
 # Set assumption for booster waning rate
 assumptions <- "central" #-log(67.7/82.8)/(105-25) # (Stowe Nat Comm 2022 Table S11)
@@ -77,24 +53,13 @@ vaccination_coverage_plot(pars$base$vaccine_schedule,pars$base$age_groups,vax,po
 dir.create("output")
 ggsave("output/vax_cov_by_dose.pdf",width = 9,height = 3)
 
-# # Set progression rates between different vaccine strata
-# vaccine_progression_rate <- c(0,0,1/(26*7),0,-log(67.7/82.8)/(105-25)) # (Stowe Nat Comm 2022 Table S11)
-# # vaccine_progression_rate <- c(0,0,1/(26*7),0,-log(0.923)/140) # (Barnard Nat Comm 2022 Table S4)
-
 # Fit covid_multi_strain to FP data
-# u <- 1:9 # all parameters
-# u <- 1:4 # only beta parameters
-# u <- c(1:4,6:9) # beta parameters, seed date, strain seed date, IHR scaling, IFR scaling
-# u <- c(1:6,8:10,13) # beta parameters, seed date, strain seed date, IHR scaling, 2nd strain seed date, observation parameters for case data
-# u <- c(1:5,7:9,12) # beta parameters, seed date, strain seed date, IHR scaling, 2nd strain seed date, observation parameters for case data
-u <- c(1:5,7:9,12:13) # beta parameters, seed date, strain seed date, IHR scaling, 2nd strain seed date, observation parameters for case data
-# u <- c(1:10,12:14,17) # beta parameters, seed date, strain seed date, IHR scaling, 2nd strain seed date, observation parameters for case data
-# u <- c(1:7,9:11,14) # beta parameters, seed date, strain seed date, IHR scaling, 2nd strain seed date, observation parameters for case data
-n_iters <- 5e4 #1e3 #2e4 #1e4 #200 #
+u <- c(1:5,7:9,12:13) # beta parameters, seed date, strain seed date, IHR scaling, 2nd strain seed date, reporting rate for confirmed cases
+n_iters <- 5e4
 # Change run number for different assumption on booster waning rate
-run <- 77 #78 #75 #76 #
+run <- 77
 # run <- 78
-deterministic <- T # flag for whether to use "deterministic particle filter" or not
+deterministic <- T # flag for whether to use deterministic model or not
 Rt <- T #F # flag for whether to return variables needed for calculating Rt in "state" object
 thinning <- 10
 fit_covid_multi_strain(data_raw,pars,u,n_iters,run,deterministic,Rt,thinning)
@@ -104,13 +69,13 @@ fit_covid_multi_strain(data_raw,pars,u,n_iters,run,deterministic,Rt,thinning)
 output <- paste0("output/MCMCoutput",run,".RData")
 
 # Set burn-in
-burnin <- 1500 #1 #
+burnin <- 1500
 
 # Set whether to plot moving average of data
 moving_avg <- F
 
 # Set number of posterior samples for age-decomposition plots
-n_smpls <- 1000 #10 #
+n_smpls <- 1000
 
 # Set seed for immune status plot
 seed <- 1L
